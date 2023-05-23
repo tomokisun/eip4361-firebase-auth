2a0e03a722159f1c064a21e821f4123775748d58
tomokisun <github@tomokisun.com>
2023-05-23 09:20:29 +0900

    Update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -20,13 +20,6 @@ app.post('/nonce', async (req, res) => {
 
   const nonce = generateNonce()
 
-  const user = await admin.firestore().doc(`/nonces/${address}`).get()
-  if (user.exists) {
-    return res.send({
-      nonce: nonce,
-    })
-  }
-
   await admin.firestore().doc(`/nonces/${address}`).set({
     nonce: nonce,
   })
